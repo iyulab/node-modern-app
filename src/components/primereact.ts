49b374cfdef05dc45aa23adbba9d6f2a3a7741f1
uj <caveman@iyulab.com>
2024-07-18 20:40:45 +0900

    update

---
+++ src/components/primereact.ts
@@ -0,0 +1,7 @@
+// primereact를 사용하는 콤포넌트는 이 파일을 import 해야합니다.
+// import "../primereact";
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css'; // flex
